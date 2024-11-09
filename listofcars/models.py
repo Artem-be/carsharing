39401a7f180d6ed72084330361ad4f45852709f6
Artem <a.k.a.aksenov@gmail.com>
2024-11-09 09:28:20 +0300

    контракт для машин

--- listofcars/models.py
+++ listofcars/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.conf import settings
-
 class Cars(models.Model):
     carid = models.IntegerField('ID',unique=True)
     make = models.CharField('Марка', max_length=255)
     model = models.CharField('Модель', max_length=255)
     year = models.IntegerField('Год выпуска')
     description = models.TextField('описание автомобиля')
+    is_reserved = models.BooleanField(default=False)
     created_at = models.DateTimeField('дата и время создания записи', auto_now_add=True)
     updated_at = models.DateTimeField('дата и время последнего обновления записи', auto_now=True)
 
@@ -20,11 +20,12 @@ class Cars(models.Model):
 
 class Contracts(models.Model):
     counthours = models.IntegerField('Кол часов')
-    car = models.ForeignKey('Cars',verbose_name='Машина', on_delete=models.CASCADE)
+    car = models.ForeignKey('Cars', verbose_name='Машина', on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.car}'
+        return f'Контракт для {self.car} от {self.author}'
 
     class Meta:
         verbose_name = 'Контракт'
--- listofcars/views.py
+++ listofcars/views.py
@@ -3,13 +3,43 @@ from django.views import View
 from .models import Cars, Contracts
 from django.contrib.auth import logout
 from .form import ContractsForm
+
+
 class CarView(View):
     def get(self, request):
-        post_car = {
-            'Cars': Cars.objects.all(),
-        }
-        return render(request, 'html/index.html', post_car)
+        # Получаем только те автомобили, у которых нет контрактов
+        available_cars = Cars.objects.filter(is_reserved=False).exclude(contracts__isnull=False)
 
+        context = {
+            'Cars': available_cars,
+        }
+        return render(request, 'html/index.html', context)
+class ReserveCarView(View):
+    def get(self, request, car_id):
+        car = Cars.objects.get(carid=car_id)
+        context = {
+            'car': car,
+            'form': ContractsForm()
+        }
+        return render(request, 'html/reserve_car.html', context)
+
+    def post(self, request, car_id):
+        user = request.user
+        car = Cars.objects.get(carid=car_id)
+        counthours = ContractsForm(request.POST)
+        if counthours.is_valid():
+            contract = Contracts.objects.create(
+                car=car,
+                author=user,
+                counthours=counthours.cleaned_data['counthours']
+            )
+            contract.save()
+            return redirect('/')  # Перенаправляем на главную страницу
+        context = {
+            'car': car,
+            'form': counthours
+        }
+        return render(request, 'html/reserve_car.html', context)
 class LogoutView(View):
     '''Выход пользователя из системы'''
     def get(self, request):
@@ -17,28 +47,3 @@ class LogoutView(View):
         return redirect('home')
 
 
-class ReserveCarView(View):
-    def get(self, request, car_id):
-        car = Cars.objects.get(carid=car_id)
-        context = {
-            'car': car,
-            'form': ContractsForm()  # Передаем пустую форму
-        }
-        return render(request, 'html/reserve_car.html', context)
-
-    def post(self, request, car_id):
-        user = request.user
-        car = Cars.objects.get(carid=car_id)
-        counthours = ContractsForm(request.POST)  # Создаем форму с данными из POST запроса
-        if counthours.is_valid():
-            counthours.save(commit=False)  # Создаем объект, но не сохраняем его
-            counthours.car = car  # Устанавливаем машину
-            counthours.author = user  # Устанавливаем пользователя
-            counthours.save()  # Сохраняем объект
-            return redirect('/')
-
-        context = {
-            'car': car,
-            'form': counthours
-        }
-        return render(request, 'html/reserve_car.html', context)
